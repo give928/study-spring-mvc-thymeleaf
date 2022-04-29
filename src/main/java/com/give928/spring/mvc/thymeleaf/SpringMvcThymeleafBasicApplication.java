fb903299de9129a03d40b3c9ecfe983a88f87223
joohokim <give928@gmail.com>
2022-04-29 23:51:22 +0900

    Initial commit

---
+++ src/main/java/com/give928/spring/mvc/thymeleaf/SpringMvcThymeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package com.give928.spring.mvc.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleafBasicApplication.class, args);
+    }
+
+}
